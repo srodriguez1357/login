beb56f6a5cc7fe6ced6c360d99b80cc0fdea8c07
Sayuri Uyeda <sayuri_uyeda@hotmail.com>
2018-11-27 08:40:38 -0600

    registro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { UsuarioPage } from '../usuario/usuario';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+personas = [];
+persona = 
+  {email: '', contra: '', nombre: '', imagen : '../assets/user.png'};
+usuario = UsuarioPage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+    public alertCtrl: AlertController) {
+
+  }
+
+  registro() {
+   if (this.persona.email.length > 0 && this.persona.contra.length > 0){
+    this.personas.push(this.usuario, {personas: this.personas});
+   } else {
+    const alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'A tu usuario le hacen falta datos',
+      buttons: ['OK']
+    });
+    alert.present();
+   }
 
   }
 
